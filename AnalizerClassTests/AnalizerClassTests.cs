567bd7c13ecd47b5664509feb483899ee798f7c5
Vitalii P <pelekhvitaliy@gmail.com>
2022-11-04 20:46:55 +0200

    Add project files.

---
+++ AnalizerClassTests/AnalizerClassTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AnalizerClassLibrary.Tests
+{
+    [TestClass]
+    public class AnalizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void EstimateTest1_Error07()
+        {
+            //Arrange
+            string expected = "&Error 07 — Дуже довгий вираз. Максимальная довжина — 65536 символів.";
+            AnalizerClass.expression = new string('9', 65537);
+
+            //Actual
+            string actualResult = AnalizerClass.Format();
+
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "data.xml", "ValidData", DataAccessMethod.Sequential)]
+        public void EstimateTest2_ValidData()
+        {
+            //Arrange
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+            AnalizerClass.expression = Convert.ToString(TestContext.DataRow["incomingData"]);
+
+            //Actual
+            string actualResult = AnalizerClass.Estimate();
+
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "data.xml", "InValidData", DataAccessMethod.Sequential)]
+        public void EstimateTest3_InValidData()
+        {
+            //Arrange
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+            AnalizerClass.expression = Convert.ToString(TestContext.DataRow["incomingData"]);
+
+            //Actual
+            string actualResult = AnalizerClass.Estimate();
+
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
---
+++ AnalizerClassTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerClassTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerClassTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ee22811-be4d-44ee-afb4-0679ab5cd132")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
